6223f50558d4d3726c6e8dacfb9be57e0f4ff81b
Erwin Corzo <erwincorzo@MacBook-Pro-de-Erwin.local>
2026-07-09 21:00:29 -0500

    fix

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 for i in $(seq 1 "$1")
 do
   # El número aleatorio debe concatenarse directamente al final de la URL
-  curl -s -o /dev/null \
+  curl  -o /dev/null \
   -H "Content-Type: application/json" \
   "http://localhost:8081/server-1/user/$(( RANDOM % 5 + 1 ))"
 done
